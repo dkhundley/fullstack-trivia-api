812ca2e6827f3c05e100a624a27a3b1b583e676a
David Hundley <david.k.hundley@gmail.com>
2020-03-09 20:55:47 -0500

    Final updates to frontend files

--- frontend/src/components/FormView.js
+++ frontend/src/components/FormView.js
@@ -17,7 +17,7 @@ class FormView extends Component {
 
   componentDidMount(){
     $.ajax({
-      url: '/categories',
+      url: `/categories`,
       type: "GET",
       success: (result) => {
         this.setState({ categories: result.categories })
@@ -34,7 +34,7 @@ class FormView extends Component {
   submitQuestion = (event) => {
     event.preventDefault();
     $.ajax({
-      url: '/questions',
+      url: `/questions`,
       type: "POST",
       dataType: 'json',
       contentType: 'application/json',
